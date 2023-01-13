75d19018f421ca1b023f3144e4cb238cb0ee990d
sophia370 <122615271+sophia370@users.noreply.github.com>
2023-01-13 14:05:28 +0100

    nu med plus ting

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+print_hi('PyCharm')
+
+print("summen er",9+9)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
